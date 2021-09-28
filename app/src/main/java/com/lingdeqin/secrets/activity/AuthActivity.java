b560b82f5dd02191f2ebffb32466a45788e6d742
lingdeqin <lingdeqin@gmail.com>
2021-09-28 22:48:49 +0800

    unlockUI

--- app/src/main/java/com/lingdeqin/secrets/activity/AuthActivity.java
+++ app/src/main/java/com/lingdeqin/secrets/activity/AuthActivity.java
@@ -12,7 +12,13 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.InputType;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.lingdeqin.secrets.R;
@@ -21,27 +27,60 @@ import java.util.concurrent.Executor;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private static final String TAG = "AuthActivity";
     private Executor executor;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
+
+    private EditText editAuth;
+    private Button btnUnlock;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        editAuth = findViewById(R.id.edit_auth);
+        btnUnlock = findViewById(R.id.btn_unlock);
+        CheckBox cb = findViewById(R.id.is_visible_password);
+
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    editAuth.setInputType(InputType.TYPE_CLASS_TEXT
+                            | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                            | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+                } else {
+                    editAuth.setInputType(InputType.TYPE_CLASS_TEXT
+                            | InputType.TYPE_TEXT_VARIATION_PASSWORD
+                            | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+                }
+            }
+        });
+
+        btnUnlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
 
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
+    }
+
+    private void biometric(){
         BiometricManager biometricManager = BiometricManager.from(this);
         switch (biometricManager.canAuthenticate(BIOMETRIC_STRONG | DEVICE_CREDENTIAL)) {
             case BiometricManager.BIOMETRIC_SUCCESS:
@@ -97,9 +136,8 @@ public class AuthActivity extends AppCompatActivity {
         promptInfo = new BiometricPrompt.PromptInfo.Builder()
                 .setTitle("需要验证")
                 .setSubtitle("验证以使用")
-                .setNegativeButtonText("使用设备PIN、密码")
+                .setNegativeButtonText("使用主密码")
                 .build();
-
         biometricPrompt.authenticate(promptInfo);
 
     }
