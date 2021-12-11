5e11beb5c4b7b38d873c63a35f043e041b865905
lingdeqin <lingdeqin@gmail.com>
2021-12-11 14:19:16 +0800

    导出修改

--- app/src/main/java/com/lingdeqin/secrets/ui/fragment/SettingsFragment.java
+++ app/src/main/java/com/lingdeqin/secrets/ui/fragment/SettingsFragment.java
@@ -1,8 +1,11 @@
 package com.lingdeqin.secrets.ui.fragment;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +13,9 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.navigation.Navigation;
@@ -17,12 +23,29 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
+import com.alibaba.fastjson.JSON;
+import com.google.android.material.snackbar.Snackbar;
 import com.lingdeqin.secrets.R;
+import com.lingdeqin.secrets.core.room.AppDatabase;
+import com.lingdeqin.secrets.core.room.entity.Secret;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleEmitter;
+import io.reactivex.rxjava3.core.SingleObserver;
+import io.reactivex.rxjava3.core.SingleOnSubscribe;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     private static final String TAG = "SettingsFragment";
-
+    private ActivityResultLauncher exportLauncher;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
@@ -85,14 +108,81 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     }
 
     private void startExport(){
-        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT)
-                .addCategory(Intent.CATEGORY_OPENABLE)
-                .setType("application/json")
-                .putExtra(Intent.EXTRA_TITLE, "export");
         try {
-            startActivityForResult(intent, 0, null);
+            exportLauncher.launch("secrets.json");
         } catch (ActivityNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        exportLauncher = registerForActivityResult(new ActivityResultForCreateDocument(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                Single.create(new SingleOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(@io.reactivex.rxjava3.annotations.NonNull SingleEmitter<Integer> emitter) throws Throwable {
+                        OutputStream outStream = null;
+                        try {
+                            outStream = getContext().getContentResolver().openOutputStream(result, "w");
+                            List<Secret> secrets = AppDatabase.getInstance().secretDao().getAll();
+                            Map<String,Object> data = new HashMap<>();
+                            data.put("secrets",secrets);
+                            outStream.write(JSON.toJSONBytes(data));
+                            emitter.onSuccess(1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            emitter.onError(e);
+                        } finally {
+                            try {
+                                if (outStream != null) {
+                                    outStream.close();
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.newThread())
+                        .subscribe(new SingleObserver<Integer>() {
+                            @Override
+                            public void onSubscribe(@io.reactivex.rxjava3.annotations.NonNull Disposable d) {}
+
+                            @Override
+                            public void onSuccess(@io.reactivex.rxjava3.annotations.NonNull Integer integer) {
+                                Snackbar.make(getView(),"导出成功",Snackbar.LENGTH_LONG).show();
+                                getParentFragmentManager().popBackStack();
+                            }
+
+                            @Override
+                            public void onError(@io.reactivex.rxjava3.annotations.NonNull Throwable e) {
+                                Snackbar.make(getView(),"导出失败:"+e.getMessage(),Snackbar.LENGTH_LONG).show();
+                            }
+                        });
+
+
+            }
+        });
+    }
+
+    public static class ActivityResultForCreateDocument extends ActivityResultContract<String, Uri> {
+
+        @NonNull
+        @Override
+        public Intent createIntent(@NonNull Context context, String input) {
+            return new Intent(Intent.ACTION_CREATE_DOCUMENT)
+                    .addCategory(Intent.CATEGORY_OPENABLE)
+                    .setType("application/json")
+                    .putExtra(Intent.EXTRA_TITLE, input);
+        }
+
+        @Override
+        public Uri parseResult(int resultCode, @Nullable Intent intent) {
+            if (intent == null || resultCode != Activity.RESULT_OK) return null;
+            return intent.getData();
         }
     }
 
