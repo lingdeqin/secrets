e338459bde6061a87e75279a2ce78c61b3db4959
lingdeqin <lingdeqin@gmail.com>
2021-12-14 18:29:03 +0800

    back

--- app/src/main/java/com/lingdeqin/secrets/ui/fragment/BackupFragment.java
+++ app/src/main/java/com/lingdeqin/secrets/ui/fragment/BackupFragment.java
@@ -1,19 +1,36 @@
 package com.lingdeqin.secrets.ui.fragment;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.navigation.Navigation;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.services.drive.DriveScopes;
 import com.lingdeqin.secrets.R;
 
 public class BackupFragment extends PreferenceFragmentCompat {
+
+    private ActivityResultLauncher googleSignLauncher;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -24,12 +41,71 @@ public class BackupFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.backup_preferences, rootKey);
+
+        //备份
+        Preference GoogleDrive = findPreference("GoogleDrive");
+        GoogleDrive.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Log.i("TAG", "onPreferenceClick: GoogleDrive=======");
+                googleSignLauncher.launch("");
+                return false;
+            }
+        });
+
+        //备份
+        Preference GoogleDriveSignOut = findPreference("GoogleDriveSignOut");
+        GoogleDriveSignOut.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                return false;
+            }
+        });
+
+
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         menu.clear();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        googleSignLauncher = registerForActivityResult(new ActivityResultForGoogleSign(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+
+            }
+        });
+
+
+    }
+
+    public static class ActivityResultForGoogleSign extends ActivityResultContract<String, Uri> {
+
+        @NonNull
+        @Override
+        public Intent createIntent(@NonNull Context context, String input) {
+            GoogleSignInOptions signInOptions =
+                    new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestEmail()
+                            .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
+                            .build();
+            GoogleSignInClient client = GoogleSignIn.getClient(context, signInOptions);
+
+            return client.getSignInIntent();
+        }
+
+        @Override
+        public Uri parseResult(int resultCode, @Nullable Intent intent) {
+            if (intent == null || resultCode != Activity.RESULT_OK) return null;
+            return intent.getData();
+        }
     }
 
 }
